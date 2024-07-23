5a0713e605aab23a7620fdf5d3bb34708c8a017b
Jan Harkes <jaharkes@cs.cmu.edu>
2024-07-23 16:35:31 -0400

    Bumping to 0.1.5.post.dev0

--- src/wireguard4netns/__init__.py
+++ src/wireguard4netns/__init__.py
@@ -4,7 +4,7 @@
 # Copyright (c) 2022-2023 Carnegie Mellon University
 # SPDX-License-Identifier: MIT
 #
-__version__ = "0.1.5"
+__version__ = "0.1.5.post.dev0"
 
 from .wireguard_daemon import create_wireguard_tunnel
 
